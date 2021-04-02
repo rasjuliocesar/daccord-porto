b0a6a1a4ec364326ef2bd470329c4d0b214bdad2
Julio Cesar <rasjuliocesar@gmail.com>
2021-04-02 08:19:01 -0300

    Início Projeto

---
+++ src/main/java/com/daccord/BackDaccordPortoApplication.java
@@ -0,0 +1,13 @@
+package com.daccord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDaccordPortoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDaccordPortoApplication.class, args);
+	}
+
+}
