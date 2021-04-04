0df1bf7573bd107dec96d923b42927a4cdd89f46
Julio Cesar <rasjuliocesar@gmail.com>
2021-04-04 19:50:57 -0300

    ENTITIES - User

---
+++ src/main/java/com/daccord/entities/User.java
@@ -0,0 +1,101 @@
+package com.daccord.entities;
+
+import com.daccord.utilities.Utilities;
+
+public class User {
+
+	private String _id;
+	private String name;
+	private String login;
+	private String password;
+	private String email;
+	private String phone;
+	
+	public User() {
+	}
+
+	public User(String name, String login, String password, String email, String phone) {
+		
+		Utilities util = new Utilities();
+		this._id = util.geradorId();
+		
+		this.name = name;
+		this.login = login;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public String get_id() {
+		return _id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_id == null) ? 0 : _id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (_id == null) {
+			if (other._id != null)
+				return false;
+		} else if (!_id.equals(other._id))
+			return false;
+		return true;
+	}
+}
