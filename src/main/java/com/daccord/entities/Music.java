a258a331ad5ccfa0f6af5824f25f5fb5744c595c
Julio Cesar <rasjuliocesar@gmail.com>
2021-04-04 20:09:13 -0300

    ENTITY - Music 'Delete método geradorID()'

--- src/main/java/com/daccord/entities/Music.java
+++ src/main/java/com/daccord/entities/Music.java
@@ -161,20 +161,6 @@ public class Music {
 		
 		return listaGenero;
 	}
-
-	/**
-	 * Gerador de id automatico de 20 caracteres.
-	 * @return id
-	 * @author Carlos.Pereira
-	 */
-	public String geradorId() {
-		Date data = new Date();
-		SimpleDateFormat dataFormatada = new SimpleDateFormat("ddMMyyyy");
-		String random = new RandomDataImpl().nextSecureHexString(12);
-		String id = dataFormatada.format(data) + random;
-		
-		return  id;
-	}
 	
 	@Override
 	public int hashCode() {
