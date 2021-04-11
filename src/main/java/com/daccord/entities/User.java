11c8518b3c4f07c94d29bc0cd88badc7e801c4e7
Julio Cesar <rasjuliocesar@gmail.com>
2021-04-11 08:34:04 -0300

    Entities - Class User

---
+++ src/main/java/com/daccord/entities/User.java
@@ -0,0 +1,76 @@
+package com.daccord.entities;
+
+public class User {
+
+	private String _id;
+	private String name;
+	private String email;
+	private String password;
+	
+	public User() {
+	}
+	
+	public User(String _id, String name, String email, String password) {
+		this._id = _id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String get_id() {
+		return this._id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_id == null) ? 0 : _id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (_id == null) {
+			if (other._id != null)
+				return false;
+		} else if (!_id.equals(other._id))
+			return false;
+		return true;
+	}
+}
