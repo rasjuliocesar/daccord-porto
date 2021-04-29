fd15a202efca59c0746f441ff3ad56a91ffe3ad3
Julio Cesar <rasjuliocesar@gmail.com>
2021-04-28 22:03:42 -0300

    @Bean App

--- src/main/java/com/daccord/BackDaccordPortoApplication.java
+++ src/main/java/com/daccord/BackDaccordPortoApplication.java
@@ -2,6 +2,9 @@ package com.daccord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackDaccordPortoApplication {
@@ -9,5 +12,15 @@ public class BackDaccordPortoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackDaccordPortoApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");;
+			}
+		};
+	}
 }
