93cc800e91ae81a4667213e80da9c73731dac0cf
Julio Cesar <rasjuliocesar@gmail.com>
2021-05-08 08:50:05 -0300

    Inicio Projeto

---
+++ old_version2/src/test/java/com/daccord/BackDaccordPortoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daccord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDaccordPortoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
